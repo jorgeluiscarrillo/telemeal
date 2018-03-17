fc701a85be4c2efe163c5c8ead1220a91128447c
Daehee <daehee6316@gmail.com>
2018-03-16 22:53:04 -0700

    layout

---
+++ app/src/main/java/com/telemeal/EditEmployeeActivity.java
@@ -0,0 +1,13 @@
+package com.telemeal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditEmployeeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_employee);
+    }
+}
