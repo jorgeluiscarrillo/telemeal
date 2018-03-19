cef0b74c3854992a70cac634b9531eb3d964d235
daehee-kang <daehee6316@gmail.com>
2018-03-19 15:55:04 -0700

    save

--- app/src/main/java/com/telemeal/LoginActivity.java
+++ app/src/main/java/com/telemeal/LoginActivity.java
@@ -7,11 +7,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
 public class LoginActivity extends AppCompatActivity {
 
     private EditText et_eid;
     private EditText et_name;
     private Button btn_enter;
+
+    private DatabaseReference dbEmployee;
+    private Query empQuery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,13 +35,29 @@ public class LoginActivity extends AppCompatActivity {
         et_name = (EditText) findViewById(R.id.login_et_ename);
         btn_enter = (Button) findViewById(R.id.login_btn_enter);
 
+        dbEmployee = FirebaseDatabase
+                .getInstance()
+                .getReference("employees");
+
         btn_enter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                getSigningInfo();
                 Intent mngrIntent = new Intent(LoginActivity.this, ManagerOptionActivity.class);
                 startActivity(mngrIntent);
             }
         });
+    }
+
+    private Employee emp;
+
+    private void getSigningInfo(){
+        String name = et_name.getText().toString();
+        int eid = Integer.parseInt(et_eid.getText().toString());
+
+
+
+        System.out.print(emp);
     }
 
 }
