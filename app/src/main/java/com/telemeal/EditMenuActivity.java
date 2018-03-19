51a30b5e9aeca8db1038ad2b965d8fa67a92335a
Daehee <daehee6316@gmail.com>
2018-03-18 23:20:02 -0700

    firebase

--- app/src/main/java/com/telemeal/EditMenuActivity.java
+++ app/src/main/java/com/telemeal/EditMenuActivity.java
@@ -3,10 +3,15 @@ package com.telemeal;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class EditMenuActivity extends AppCompatActivity {
 
@@ -29,6 +34,8 @@ public class EditMenuActivity extends AppCompatActivity {
     private Button btn_edit_fd;
     private Button btn_delete_fd;
 
+    private DatabaseReference dbFoods;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +44,8 @@ public class EditMenuActivity extends AppCompatActivity {
     }
 
     private void initializer() {
+        dbFoods = FirebaseDatabase.getInstance().getReference("foods");
+
         et_name = (android.widget.EditText) findViewById(R.id.edfd_et_name);
         et_price = (EditText) findViewById(R.id.edfd_et_price);
         et_desc = (EditText) findViewById(R.id.edfd_et_desc);
@@ -59,7 +68,7 @@ public class EditMenuActivity extends AppCompatActivity {
         btn_add_fd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                addFood();
             }
         });
 
@@ -74,14 +83,14 @@ public class EditMenuActivity extends AppCompatActivity {
         btn_edit_fd.setOnClickListener((new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                editFood();
             }
         }));
 
         btn_delete_fd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                deleteFood();
             }
         });
 
@@ -98,5 +107,31 @@ public class EditMenuActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void addFood(){
+        String name = et_name.getText().toString();
+        double price = Double.parseDouble(et_price.getText().toString());
+        String desc = et_desc.getText().toString();
+        String image = et_image.getText().toString();
+        String category = spnr_category.getSelectedItem().toString();
+
+        if(!TextUtils.isEmpty(name)){
+            String id = dbFoods.push().getKey();
+
+            Food food = new Food(name, price, desc, image, category);
+
+            dbFoods.child(id).setValue(food);
+
+            Toast.makeText(this, "Food " + name + " added", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void editFood(){
+
+    }
+
+    private void deleteFood(){
+
     }
 }
