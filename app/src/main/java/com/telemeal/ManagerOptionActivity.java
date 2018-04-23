9e3f8e74aed492fdb384eaaf8f7f04d20ca25c13
ChaozFire <Bryandu0ng@yahoo.com>
2018-04-23 16:35:33 -0700

    Added Preliminary Order Layout + Fixed error w/ Checkout

--- app/src/main/java/com/telemeal/ManagerOptionActivity.java
+++ app/src/main/java/com/telemeal/ManagerOptionActivity.java
@@ -11,6 +11,7 @@ public class ManagerOptionActivity extends AppCompatActivity {
     private Button btn_employee;
     private Button btn_menu;
     private Button btn_invoice;
+    private Button btn_order;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +24,7 @@ public class ManagerOptionActivity extends AppCompatActivity {
         btn_employee = (Button) findViewById(R.id.mngr_btn_editemp);
         btn_menu = (Button) findViewById(R.id.mngr_btn_editfd);
         btn_invoice = (Button) findViewById(R.id.mngr_btn_invoice);
+        btn_order = (Button) findViewById(R.id.mngr_btn_orders);
 
         btn_employee.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,5 +41,15 @@ public class ManagerOptionActivity extends AppCompatActivity {
                 startActivity(fdIntent);
             }
         });
+
+        btn_order.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ordIntent = new Intent(ManagerOptionActivity.this, OrdersActivity.class);
+                startActivity(ordIntent);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/telemeal/OrdersActivity.java
+++ app/src/main/java/com/telemeal/OrdersActivity.java
@@ -2,7 +2,17 @@ package com.telemeal;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.LinearLayout;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 /**
  * Created by Bryan on 4/18/2018.
@@ -10,12 +20,41 @@ import android.support.v7.widget.RecyclerView;
 
 public class OrdersActivity extends AppCompatActivity {
     private RecyclerView ordersList;
+    private ArrayList<Order> orders;
+    private ArrayList<UploadImage> images;
+    DatabaseReference dbImages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_orders);
+        orders = new ArrayList<>();
+        images = new ArrayList<>();
+
+        dbImages = FirebaseDatabase
+                .getInstance()
+                .getReference("image");
+
+        dbImages.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                images.clear();
+                for(DataSnapshot postSnapshot: dataSnapshot.getChildren())
+                {
+                    UploadImage image = postSnapshot.getValue(UploadImage.class);
+                    images.add(image);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
         ordersList = (RecyclerView) findViewById(R.id.orders);
+        OrderAdapter adapter = new OrderAdapter(getApplicationContext(), orders, images);
+        ordersList.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        ordersList.setAdapter(adapter);
     }
 }
